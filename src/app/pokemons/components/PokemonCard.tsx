5ab64ef1484848b5887733d0d8031b7986e0f738
Sebastian <sebastianvalbuena25@gmail.com>
2024-01-02 21:27:08 -0500

    Generando paginas dinamicas

---
+++ src/app/pokemons/components/PokemonCard.tsx
@@ -0,0 +1,44 @@
+import Image from "next/image"
+import Link from "next/link"
+import { SimplePokemon } from ".."
+import { IoHeartOutline } from "react-icons/io5"
+
+interface Props {
+    simplePokemon: SimplePokemon
+}
+
+export const PokemonCard = ({ simplePokemon }: Props) => {
+    const { id, name } = simplePokemon;
+
+    return (
+        <div className="mx-auto right-0 mt-2 w-60">
+            <div className="flex flex-col bg-white rounded overflow-hidden shadow-lg">
+                <div className="flex flex-col justify-center items-center text-center p-6 bg-gray-800 border-b">
+                    <Image priority={false} src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/${id}.svg`} width={100} height={100} alt={name} />
+                    <p className="pt-2 text-lg font-semibold text-gray-50 capitalize">{name}</p>
+                    <div className="mt-5">
+                        <Link
+                            href={`pokemon/${id}`}
+                            className="border rounded-full py-2 px-4 text-xs font-semibold text-gray-100"
+                        >
+                            Read more
+                        </Link>
+                    </div>
+                </div>
+                <div className="border-b">
+                    <Link href="/dashboard/main" className="px-4 py-2 hover:bg-gray-100 flex items-center">
+                        <div className="text-green-600">
+                            <IoHeartOutline className="text-red-600" />
+                        </div>
+                        <div className="pl-3">
+                            <p className="text-sm font-medium text-gray-800 leading-none">
+                                Not is favorite
+                            </p>
+                            <p className="text-xs text-gray-500">View your campaigns</p>
+                        </div>
+                    </Link>
+                </div>
+            </div>
+        </div>
+    )
+}
---
+++ src/app/shopping-cart/components/CartCounter.tsx
@@ -0,0 +1,23 @@
+'use client';
+
+import { useState } from "react";
+
+interface Props {
+    value: number;
+}
+
+export const CartCounter = ({ value }: Props) => {
+    const [counter, setCounter] = useState(value);
+    return (
+        <>
+            <div>CartCounter</div>
+            <span className="text-9xl">{counter}</span>
+
+            <div className="flex">
+                <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" onClick={() => setCounter(counter + 1)}>+1</button>
+
+                <button className="flex items-center justify-center p-2 rounded-xl bg-gray-900 text-white hover:bg-gray-600 transition-all w-[100px] mr-2" onClick={() => setCounter(counter - 1)}>-1</button>
+            </div>
+        </>
+    )
+}
